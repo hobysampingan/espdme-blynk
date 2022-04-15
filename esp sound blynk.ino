376532e958ab83c9f8f3c13c3074a1427ddf3618
HOBY SAMPINGAN <103373678+hobysampingan@users.noreply.github.com>
2022-04-16 02:02:46 +0700

    Create esp sound blynk.ino

---
+++ esp sound blynk.ino
@@ -0,0 +1,64 @@
+
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "j8us3HaIlu9oOEk3x3CvJ9-gRH6W_VFB";
+char ssid[] = "izzabags-id";                       // Your WiFi credentials.
+char pass[] = "passworde123sampe8";
+
+
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+  unsigned long startMillis= millis();  // Start of sample window
+   unsigned int peakToPeak = 0;   // peak-to-peak level
+
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+
+   // collect data for 50 mS
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(0);
+      if (sample < 1024)  // toss out spurious readings
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // save just the max levels
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // save just the min levels
+         }
+      }
+   }
+   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+   double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+   Serial.println(peakToPeak);
+   Serial.println(volts);
+   Blynk.virtualWrite(V0, volts);
+   Blynk.virtualWrite(V1, peakToPeak);
+   
+}
